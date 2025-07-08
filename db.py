50c7d6621c62c8408d2456e18a4024711ae0d0c0
hamzasafdar43 <141044593+hamzasafdar43@users.noreply.github.com>
2025-07-08 16:59:11 +0500

    db.py update

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Update with your actual MySQL credentials and DB name
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Hamza%40123@localhost/testdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://alihamza:Hamza%40123@alihamza.mysql.pythonanywhere-services.com/alihamza$testdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
