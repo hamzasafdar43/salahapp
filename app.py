1afb7666cbef6d77f382098caa13db8909722cf3
hamzasafdar43 <alihamza4443243@gmail.com>
2025-07-21 09:30:55 +0000

    Resolved merge conflicts

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
+
 from flask import Flask
-from db import db  # db imported from db.py
+from db import db  # Import db from separate file
 from routes.story import story_bp
-from routes.users import users
-
-
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Hamza%40123@localhost/testdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://alihamza43:Hamza%40123@alihamza43.mysql.pythonanywhere-services.com/alihamza43$default'  # Or your actual database URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)  # Properly initialize db here
+db.init_app(app)  # Initialize db with the app
 
 app.register_blueprint(story_bp)
-app.register_blueprint(users)
+
 
 with app.app_context():
     # db.drop_all()
@@ -20,3 +18,6 @@ with app.app_context():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
