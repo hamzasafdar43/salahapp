aa5c3d2b6bb8a4f7133c2450c3edcdbb2b4b3e7c
hamzasafdar43 <141044593+hamzasafdar43@users.noreply.github.com>
2025-07-23 18:50:57 +0500

    updated

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from routes.users import users
 
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Hamza%40123@localhost/testdb'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://alihamza43:Hamza%40123@alihamza43.mysql.pythonanywhere-services.com/alihamza43$default'  # Or your actual database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Hamza%40123@localhost/testdb'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://alihamza43:Hamza%40123@alihamza43.mysql.pythonanywhere-services.com/alihamza43$default'  # Or your actual database URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)  # Properly initialize db here
